86749c6743975b47e38148630a6e27eafffcbfaf
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-08-26 07:43:56 +0200

    Remove unnedeed dependencies

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './modules/App/App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter, Route } from 'react-router-dom'
 import './index.css'
 
 render(
-	<BrowserRouter>
-		<Route path={'/'} component={App} />
-	</BrowserRouter>,
+		<App/>,
 	document.getElementById('root')
 );
 
